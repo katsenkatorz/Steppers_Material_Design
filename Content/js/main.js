3291b9a5d1fbae053dac3421844eb9e1d27c00d4
katsenkatorz <lefloch.jf@gmail.com>
2015-12-03 22:11:45 +0100

    Ajout de jquery et validate.js

--- Content/js/main.js
+++ Content/js/main.js
@@ -1,4 +1,15 @@
-window.onload = function () {
+$(function () {
+    //  +------------------------------------------------------------------+
+    //  Init variables
+    //  +------------------------------------------------------------------+
+    var current = 1;
+    var btnNext = $(".next");
+    var btnBack = $(".back");
+    var stepContent = $(".katsenkatorz-stepper-content");
+
+
+    // Init  UI
+    stepContent.not(':eq(0)').hide();
 
     //  +------------------------------------------------------------------+
     //  Calc width of steppers
@@ -14,9 +25,35 @@ window.onload = function () {
                 if (stepHeader[i].childNodes[j].className.indexOf(step) > -1)
                     tab.push(stepHeader[i].childNodes[j]);
         }
-        for (var j = 0; j < tab.length; j++){
+        for (var j = 0; j < tab.length; j++) {
             tab[j].style.width = (100 / tab.length) + "%";
         }
-
     }
-};
+
+    //  +------------------------------------------------------------------+
+    //  Next Button
+    //  +------------------------------------------------------------------+
+    btnNext.click(function () {
+        if (current < stepContent.length) {
+            // Check validation
+            if ($("form").valid()) {
+                stepContent.show();
+                stepContent.not(':eq(' + (current++) + ')').hide();
+            }
+        }
+    });
+
+    //  +------------------------------------------------------------------+
+    //  Back Button
+    //  +------------------------------------------------------------------+
+    btnBack.click(function(){
+        if(current > 1){
+            current = current - 2;
+            if(current < stepContent.length){
+                stepContent.show();
+                stepContent.not(':eq('+(current++)+')').hide();
+            }
+        }
+    })
+
+});
